2abbe2d17a1adbe1115c2cbc14d92b1895782033
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-10-22 14:39:46 -0300

    criei meu primeiro test, adicionando o arquivo, .env, pasta infra, jest.confi, jsconfig, e endpoint de status

---
+++ tests/integrations/api/v1/status/get.test.js
@@ -0,0 +1,29 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(responseBody.updated_at).toBeDefined();
+      new Date(responseBody.updated_at).toISOString();
+
+      const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+      expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+      expect(responseBody.dependencies.database.version).toEqual("16.4");
+
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
+});
